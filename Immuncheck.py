82e49598fd51fe5af657ef950561eaf45b146c9b
Agnesaaaaaa <157700222+Agnesaaaaaa@users.noreply.github.com>
2024-04-09 20:57:32 +0200

    Update Immuncheck.py

--- Immuncheck.py
+++ Immuncheck.py
@@ -2,26 +2,27 @@ import streamlit as st
 import pandas as pd
 
 # Titel der App
-st.title('AgBi-Immuncheck - Digitaler Impfpass')
+st.title('Agbi-Immuncheck - Digitaler Impfpass')
 
 # Datenframe für Impfdaten erstellen oder laden, falls vorhanden
 @st.cache
 def load_data():
-    return pd.DataFrame(columns=['Datum der Impfung', 'Impfstoff'])
+    return pd.DataFrame(columns=['Kategorie', 'Datum', 'Information'])
 
 data = load_data()
 
 # Benutzeroberfläche für Hinzufügen von Impfdaten
 st.header('Impfdaten hinzufügen')
-date_of_vaccination = st.date_input('Datum der Impfung', value=pd.Timestamp.today())
-vaccine_type = st.text_input('Impfstoff')
 
-if st.button('Impfdaten hinzufügen'):
-    date_of_vaccination = pd.Timestamp(date_of_vaccination)  # Datum in Pandas Timestamp-Objekt umwandeln
-    data = data.append({'Datum der Impfung': date_of_vaccination, 'Impfstoff': vaccine_type}, ignore_index=True)
-    st.success('Impfdaten erfolgreich hinzugefügt!')
+category = st.selectbox('Kategorie wählen', ['Meine Impfungen', 'Symptome', 'Schweizerischer Impfplan'])
+date_of_event = st.date_input('Datum', value=pd.Timestamp.today())
+information = st.text_input('Information')
+
+if st.button('Eintrag hinzufügen'):
+    data = data.append({'Kategorie': category, 'Datum': date_of_event, 'Information': information}, ignore_index=True)
+    st.success('Eintrag erfolgreich hinzugefügt!')
 
 # Benutzeroberfläche für Anzeigen von Impfdaten
-st.header('Gespeicherte Impfdaten')
+st.header('Gespeicherte Daten')
 st.dataframe(data)
 
